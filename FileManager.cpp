6b33110bef6c2210b638f36861c838ef3df7db24
Nishchal-nicolas14 <sciencelife354@gmail.com>
2026-06-25 00:09:38 +0530

    Writing Workflow for .nca File Completed

--- FileManager.cpp
+++ FileManager.cpp
@@ -51,7 +51,7 @@ string FileManager::readFile(const string& file_path) {
     return content;
 }
 
-void FileManager::writeFile(const string& file_path, const string& data) {
+void FileManager::writeFile(const string& file_path, const string& data, const unordered_map<char, int>& freq) {
     // getting the file name:-
     string file_name = "";
     size_t pos1 = file_path.find_last_of('/');
@@ -71,7 +71,29 @@ void FileManager::writeFile(const string& file_path, const string& data) {
         if(!file) {
             throw runtime_error("Unable to Create File");
         }
-        file << data;
+        // function call to write the header of the file:-
+        writeFrequencyTable(file, freq);
+        // function call to write the padding(extra bits) in the data section:-
+        writePadding(file, data);
+        // bit-packing of the data:
+        uint8_t packedBits = 0;
+        int count = 8;
+        for(auto c : data) {
+            if(count == 0) {
+                file.write(reinterpret_cast<const char*>(&packedBits), sizeof(packedBits));
+                packedBits = 0;
+                count = 8;
+            }
+            packedBits <<= 1;
+            if(c == '1') {
+                packedBits = packedBits | 1;
+            }
+            count--;
+        }
+        if(count) {
+            packedBits <<= count;
+            file.write(reinterpret_cast<const char*>(&packedBits), sizeof(packedBits));
+        }
         file.close();
     }
     else if(extension == ".nca") {
@@ -87,4 +109,29 @@ void FileManager::writeFile(const string& file_path, const string& data) {
     else {
         throw runtime_error("File Extension not Found!");
     }
+}
+
+// helping function - 1 to write the header of the file:
+void FileManager::writeFrequencyTable(ofstream& file, const unordered_map<char, int>& freq) {
+    /*
+        static_cast<type>(expression)
+        this static_cast is used to explicitly convert expression into given type
+    */
+    uint16_t size = static_cast<uint16_t>(freq.size());
+    /*
+        reinterpret_cast<pointer>(expression address)
+        this reinterpret_cast is used to treat a collection of bytes(or bits) as completely different, and convert it into the conversion pointer
+    */
+    file.write(reinterpret_cast<char*>(&size), sizeof(size));
+    
+    for(auto& [key, value]:freq) {
+        file.write(reinterpret_cast<const char*>(&key), sizeof(key));
+        file.write(reinterpret_cast<const char*>(&value), sizeof(value));
+    }
+}
+
+// helping function - 2 to write the padding of the file:
+void FileManager::writePadding(ofstream& file, const string& data) {
+    uint8_t padding = (8 - (data.size() % 8)) % 8;
+    file.write(reinterpret_cast<const char*>(&padding), sizeof(padding));
 }
